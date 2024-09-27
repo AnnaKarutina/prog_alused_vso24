ae1e1db974aef7246ade28557b613121a709c531
Anna Karutina <anna.karutina@khk.ee>
2024-09-27 09:22:00 +0300

    Ylesanne 2.1

---
+++ yl2.1.js
@@ -0,0 +1,15 @@
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Sisesta 6hutemperatuur: ', temperatuur => {
+    if (temperatuur < 4.0) {
+        console.log('On jaatumise oht.')
+    } else {
+        console.log('Ei ole jaatumise ohtu.')
+    }
+    rl.close()
+})
