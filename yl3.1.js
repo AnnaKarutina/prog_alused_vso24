026b8fae51a36d0ef217823c670e98e4d27f6dfe
Anna Karutina <anna.karutina@khk.ee>
2024-10-03 09:38:55 +0300

    Ylesanne 3.1 - for tsükliga

---
+++ yl3.1.js
@@ -0,0 +1,13 @@
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Sisesta mitu korda äratada: ', aratamisKordus => {
+  for (let kord = 1; kord <= aratamisKordus; kord++){
+    console.log('Tõuse ja sära!')
+  } 
+  rl.close()
+})
